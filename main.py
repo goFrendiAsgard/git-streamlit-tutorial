959ed118e3892df67e33684967a0f52d5fb7862a
Atika <atika.faizah89@gmail.com>
2023-06-09 14:25:18 +0700

    add filter

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import plotly.express as px
 
-# Load Asia's GDP
-df = px.data.gapminder().query("continent=='Asia'")
+continent = st.selectbox(label='Benua', options=['Asia', 'Africa', 'Americas', 'Europe', 'Oceania'])
 
-st.title('Asia GDP from 1952-2007')
+# Load Asia's GDP
+df = px.data.gapminder().query(f"continent=='{continent}'")
+
+st.title(f'{continent} GDP from 1952-2007')
 st.table(df)
